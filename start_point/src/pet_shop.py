8ab45399db920d6ddd271b383ff6bffbfcbd5f93
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-08 19:15:39 +0000

    customer can/can not afford pet added

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -79,3 +79,12 @@ def get_customer_pet_count(customer):
 
 def add_pet_to_customer(customer, new_pet):
     customer["pets"].append(new_pet)
+
+
+def customer_can_afford_pet(customer, new_pet):
+    decision = False
+
+    if get_customer_cash(customer) >= new_pet["price"]:
+        decision = True
+
+    return decision
