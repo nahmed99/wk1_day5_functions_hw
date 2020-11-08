1454540fe94e43e032669ec2fb76d425c509fa72
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-11-08 19:56:14 +0000

    sell pet to customer, found and not found

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -88,3 +88,18 @@ def customer_can_afford_pet(customer, new_pet):
         decision = True
 
     return decision
+
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+
+    if pet is None:
+        return
+
+    if customer_can_afford_pet(customer, pet):
+        add_pet_to_customer(customer, pet)
+        increase_pets_sold(pet_shop, 1)
+        remove_customer_cash(customer, pet["price"])
+        #remove_pet_by_name(pet_shop, pet["name"])
+        add_or_remove_cash(pet_shop, pet["price"])
+        
+    
